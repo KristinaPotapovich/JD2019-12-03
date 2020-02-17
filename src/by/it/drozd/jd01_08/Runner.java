92790495be86047d1c53d484ce5120556f86cdf8
ApProve1909 <finch2215@gmail.com>
2020-02-17 19:57:10 +0300

    jd01_08 Drozd

--- src/by/it/drozd/jd01_08/Runner.java
+++ src/by/it/drozd/jd01_08/Runner.java
@@ -4,6 +4,7 @@ class Runner {
     public static void main(String[] args) {
         Var s = new Scalar(3.0);
         Var v=new Vector(new double[]{1,2,3});
+        //Var m = new Matrix("{{1,2,3},{4,5,6},{7,8,9}}");//
         System.out.println(s.add(s));
         System.out.println(s.sub(s));
         System.out.println(s.mul(s));
