1790c746c9fa16af93b07b2ceb4dae7512ccdf9b
ApProve1909 <finch2215@gmail.com>
2020-02-17 15:49:19 +0300

    jd01_08

---
+++ src/by/it/drozd/jd01_08/Runner.java
@@ -0,0 +1,17 @@
+package by.it.drozd.jd01_08;
+
+class Runner {
+    public static void main(String[] args) {
+        Var s = new Scalar(3.0);
+        Var v=new Vector(new double[]{1,2,3});
+        System.out.println(s.add(s));
+        System.out.println(s.sub(s));
+        System.out.println(s.mul(s));
+        System.out.println(s.div(s));
+
+        System.out.println(v.add(v));
+        System.out.println(v.sub(v));
+    }
+
+
+}
