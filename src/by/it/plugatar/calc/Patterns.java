ccf97852cfd7f0764a3aa2189e618fc9513d7848
henadzi.pluhatar <henadzi.pluhatar@gmail.com>
2020-03-04 06:17:11 +0300

    [+]: For Task JD02_04;

--- src/by/it/plugatar/calc/Patterns.java
+++ src/by/it/plugatar/calc/Patterns.java
@@ -1,8 +1,12 @@
 package by.it.plugatar.calc;
 
 public class Patterns {
-    static final String OPERATION="[-=+*/]";
+    static final String BRACKETS = "\\([-0-9+*/a-zA-Z.{},_ ]+\\)";
+    static final String OPERATION="(?<=[^-+*/={,])[-+*/=]";//"[-=+*/]"
     static final String SCALAR="-?[0-9]+(\\.[0-9]+)?";//"-?[0-9]+(.[0-9]+)?";//;//
-    static final String VECTOR ="\\{(" + SCALAR + ",?\\s?)+}"; //"\\{"+SCALAR+"(,"+SCALAR+")*}";//;//;
-    static final String MATRIX = "\\{(" + VECTOR + ",?\\s?)+}";//"\\{(" + VECTOR + ",?\\s?)+}";
+    static final String VECTOR="\\{"+SCALAR+"(,"+SCALAR+")*}"; //"\\{"+SCALAR+"(,"+SCALAR+")*}";//;//;
+    static final String MATRIX="\\{"+VECTOR+"(,"+VECTOR+")*}";//"\\{(" + VECTOR + ",?\\s?)+}";
+
+    private Patterns() {
+    }
 }
