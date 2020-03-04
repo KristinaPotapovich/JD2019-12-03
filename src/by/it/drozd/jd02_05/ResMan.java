bbfa00535e94cb6daa7e471f6c04ab462ce93d55
ApProve1909 <finch2215@gmail.com>
2020-03-04 15:51:40 +0300

    Drozd

--- src/by/it/drozd/jd02_05/ResMan.java
+++ src/by/it/drozd/jd02_05/ResMan.java
@@ -4,24 +4,17 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 enum ResMan {
-
     INSTANCE;
-
     private String locationRes = "by.it.drozd.jd02_05.properties.messages";
-
     private ResourceBundle resourceBundle;
-
     ResMan() {
         setLocale(Locale.ENGLISH);
     }
-
     void setLocale(Locale locale) {
         resourceBundle = ResourceBundle.getBundle(locationRes, locale);
     }
-
     String get(String key) {
         return resourceBundle.getString(key);
     }
-
 
 }
