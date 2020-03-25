0ca0a80ab58e395e6b45cbb6c588ac28f20d3f03
kristina potapovich <ryzhik1191@gmail.com>
2020-03-25 23:32:44 +0300

    TaskB1

--- src/by/it/potapovich/jd01_12/TaskB1.java
+++ src/by/it/potapovich/jd01_12/TaskB1.java
@@ -8,89 +8,24 @@ import java.util.*;
 public class TaskB1 {
     public static void main(String[] args) {
         Map<String, Integer> hashMap = new LinkedHashMap<>();
-        //List<String> arr = new ArrayList<>();
         Scanner sc = new Scanner(System.in).useDelimiter("\n");
         String text = "";
         int i = 0;
         while (sc.hasNextLine()) {
-          //  System.out.println(i++);
             String str = sc.nextLine();
-
-            //System.out.println("--------------------------------------------------------");
-     //       System.out.println(str);
-         //   System.out.println("--------------------------------------------------------");
-       //     System.out.println();
-
-            if (str.isEmpty()){
+            if (str.isEmpty()) {
                 continue;
             }
-
-
-           // str = str.trim();
             str = str.replaceAll("[.\\-,—?\"!;:/]", " ");
             str = str.replaceAll("\n+|\r\n+", " ");
             str = str.replaceAll("[\\s]{2,}", " ");
-
-
-            if (str.equals("end")){
-                text = text.concat(" "+str);
+            if (str.equals("end")) {
+                text = text.concat(" " + str);
                 break;
             }
-
             text = text.concat(str + " ");
-
-            //    String[] array = str.split(" ");
-
-
-//             for (String s : array) {
-//                System.out.println(s);
-//             }
-
-            //s = s.replaceAll("\\p{Punct}", " ");
-            // s= s.replaceAll("\n+|\r\n+", " ");
-            // s = s.replaceAll("[\\s]{2,}", " ");
-
-            // for (int i = 0; i < words.length; i++) {
-            //   for (int j = 0; j < words.length; j++) {
-            //   String word = words[i];
-            //  String wordSecond = words[j];
-            // System.out.println(word);
-            // Integer pos = 0;
-            //while (!word.equals("end")) {
-
-
-
-   /*
-            for (String word : array) {
-                if (!hashMap.containsKey(word)) {
-                    hashMap.put(word, 1);
-                } else if (hashMap.containsKey(word)) {
-                    hashMap.put(word, hashMap.get(word) + 1);
-              //  }else if (word.equals("end")){
-                 //   break;
-                }
-*/
-
-
-
-
-                // for ( word : hashMap.keySet();
-
-
-                //}
-                //  System.out.println(word);
-                // System.out.println(word + "=" +hashMap.get(word));
-
-
-      //      }
         }
-       // System.out.println(text);
-
         String[] array = text.split(" ");
-
-
-     //   System.out.println(words);
-
         for (String word : array) {
             if (word.isEmpty()) {
                 continue;
@@ -99,17 +34,15 @@ public class TaskB1 {
             if (!hashMap.containsKey(word)) {
                 hashMap.put(word, 1);
             } else if (hashMap.containsKey(word)) {
-                if (word.equals("isn't")){
+                if (word.equals("isn't")) {
                     hashMap.put("don't", hashMap.get("don't") + 1);
                     continue;
                 }
                 hashMap.put(word, hashMap.get(word) + 1);
-                //  }else if (word.equals("end")){
-                //   break;
             }
         }
         System.out.println(hashMap);
-        }
+    }
 }
 
 
@@ -118,5 +51,4 @@ public class TaskB1 {
 
 
         
- //   }
-//}
+
